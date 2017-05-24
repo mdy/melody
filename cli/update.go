7e43d6edd49b00a9639d1003192aa04676543354
Michael Rykov <mrykov@gmail.com>
2017-05-24 21:37:53 +0800

    Allow glob patterns for "update"

--- cli/update.go
+++ cli/update.go
@@ -2,11 +2,13 @@ package cli
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"github.com/gobwas/glob"
 	"github.com/mdy/melody/project"
 	"github.com/mdy/melody/provider"
 	"github.com/mdy/melody/resolver"
 	"github.com/urfave/cli"
 	"os"
+	"strings"
 )
 
 func update(c *cli.Context) error {
@@ -21,9 +23,15 @@ func update(c *cli.Context) error {
 	if len(c.Args()) == 0 {
 		baseGraph = resolver.NewGraph()
 	} else {
+		g, err := glob.Compile("{" + strings.Join(c.Args(), ",") + "}")
+		if err != nil {
+			return err
+		}
 		baseGraph = project.Locked.Dup()
-		for _, name := range c.Args() {
-			baseGraph.DetachVertexNamed(name)
+		for _, spec := range project.Locked.Specifications() {
+			if name := spec.Name(); g.Match(name) {
+				baseGraph.DetachVertexNamed(name)
+			}
 		}
 	}
 
